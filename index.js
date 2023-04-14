81d700acf6abb67092e96503009be659d4aaeb39
rami-sy <129196519+rami-sy@users.noreply.github.com>
2023-04-14 14:59:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
